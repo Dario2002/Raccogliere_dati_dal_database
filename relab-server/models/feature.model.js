e1912657c78bb6e773f95b1b5a435a497673ebfe
Dario2002 <banfi.dario@iisgalvanimi.edu.it>
2020-05-06 12:15:01 +0000

    API REST

--- relab-server/models/feature.model.js
+++ relab-server/models/feature.model.js
@@ -1,14 +1,11 @@
-//Classe utile a rappresentare le feature degli oggetti geojson
-module.exports = class Feature{
+module.exports = class Feature {
     constructor(id, geometry) {
         this.type = "Feature";
-        this.properties = new Properties(id); //Per ora le proprietà contengono un solo valore (id)
-        this.geometry = geometry; //Contiene la geometria del poligono.
+        this.properties = new Properties(id);
+        this.geometry = geometry;
     }
 }
 
-
-//Iniziamo a preparare la classe Properties che complicheremo in seguito
 class Properties
 {
     constructor(id)
