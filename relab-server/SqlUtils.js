d0390f020e5df45ac9149f916c77dccff7e399d8
Dario2002 <banfi.dario@iisgalvanimi.edu.it>
2020-05-06 12:59:07 +0000

    Forse Fine?

--- relab-server/SqlUtils.js
+++ relab-server/SqlUtils.js
@@ -11,15 +11,16 @@ const config = {
 }
 
 module.exports = class SqlUtils {
-static connect(res, connectedCallback)
-    {
-        sql.connect(config, (err) => {
-            if (err) console.log(err);  // ... error check
-            else connectedCallback(res);     //callback da eseguire in caso di connessione avvenuta 
-        });
-    }
 
-    static makeSqlRequest(res) {
+    static connect(req, res, connectedCallback)
+{
+    sql.connect(config, (err) => {
+        if (err) console.log(err);  // ... error check
+        else connectedCallback(req,res); //callback da eseguire in caso di connessione avvenuta 
+    });
+}
+
+    static makeSqlRequest(req,res) {
         let sqlRequest = new sql.Request();  //sqlRequest: oggetto che serve a eseguire le query
         let q = 'SELECT DISTINCT TOP (100) [GEOM].STAsText() FROM [Katmai].[dbo].[interventiMilano]';
         //eseguo la query e aspetto il risultato nella callback
@@ -31,13 +32,14 @@ static connect(res, connectedCallback)
         if (err) console.log(err); // ... error checks
         res.send(coordConverter.generateGeoJson(result.recordset));  //Invio il risultato al Browser
     }
-    static ciVettRequest(res) {
+    static ciVettRequest(req,res) {
         let sqlRequest = new sql.Request();  //sqlRequest: oggetto che serve a eseguire le query
+        let foglio = req.params.foglio; //ottengo il foglio passato come parametro dall'url
         let q = `SELECT INDIRIZZO, WGS84_X, WGS84_Y, CLASSE_ENE, EP_H_ND, CI_VETTORE, FOGLIO, SEZ
         FROM [Katmai].[dbo].[interventiMilano]
-        WHERE FOGLIO = 90`
+        WHERE FOGLIO = ${foglio}`
         //eseguo la query e aspetto il risultato nella callback
-       sqlRequest.query(q, (err, result) => {SqlUtils.sendCiVettReult(err,result,res)}); 
+        sqlRequest.query(q, (err, result) => {SqlUtils.sendCiVettReult(err,result,res)}); 
     }
 
   static sendCiVettReult(err,result, res)
@@ -46,5 +48,3 @@ static connect(res, connectedCallback)
         res.send(result.recordset);  //Invio il risultato al Browser
   }
 }
-
-
--- relab-server/app.js
+++ relab-server/app.js
@@ -8,14 +8,15 @@ app.get('/', function (req, res) {
    //Per connettermi al DB uso il metodo statico sqlUtils.connect
    //Passo come parametro la funzione sqlUtils.makeSqlRequest che verrà lanciata 
    //se la connessione al DB avrà successo  
-   sqlUtils.connect(res, sqlUtils.makeSqlRequest);
+   sqlUtils.connect(req,res, sqlUtils.makeSqlRequest);
 });
-
-app.get('/ci_vettore', function (req, res) {
-    //richiamo il metodo che ottiene l'elenco dei vettori energetici
-    sqlUtils.connect(res, sqlUtils.ciVettRequest);
- });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+app.get('/ci_vettore/:foglio', function (req, res) {
+    console.log(req.params.foglio);
+    //richiamo il metodo che ottiene l'elenco dei vettori energetici
+    sqlUtils.connect(req,res, sqlUtils.ciVettRequest);
+ });
